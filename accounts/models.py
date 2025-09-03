72748a0aec24c43ce9534e7ae2089271ce5b6a03
Sanjiv Thapa <thapasvt12@gmail.com>
2025-09-03 21:31:40 +0545

    🐛 Fix bug while creating superuser name not prompting and added type hints

--- accounts/models.py
+++ accounts/models.py
@@ -3,21 +3,31 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email: str, name: str, password: str | None = None):
+        """
+        Create and return a regular user.
+        """
         if not email:
             raise ValueError("Users must have an email address")
+        if not name:
+            raise ValueError("Users must have a name")
+
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name, password):
-        user = self.create_user(email, name, password)
+    def create_superuser(self, email: str, name: str, password: str):
+        """
+        Create and return a superuser.
+        """
+        user = self.create_user(email=email, name=name, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
@@ -27,11 +37,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
 
     def __str__(self):
         return self.email
-
-
-
